1b4409953219c52caf36badad3721c5d5c52e0ef
yingzxu <c114245678>
2015-01-25 11:30:34 -0800

    Reorg

---
+++ MarkdownDom/Elements/HeaderElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Net.Yingzxu.MarkdownDom.Base;
+
+namespace Net.Yingzxu.MarkdownDom.Elements
+{
+    class HeaderElement : MarkdownElementBase
+    {
+
+        public int Size { set; get; }
+        public HeaderElement()
+            : base(ElementType.Header)
+        {
+        }
+
+        protected override string ToStringInternal()
+        {
+            return "HeaderSize:" + Size;
+        }
+    }
+}
